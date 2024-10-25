3b0b6c1875fde6945a621aca4d0ddc60e224d722
ASalami99 <115806577+ASalami99@users.noreply.github.com>
2024-10-25 07:23:53 +0100

    Props

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { Text, View } from "react-native";
 import { ProfileCard } from "./components/ProfileCard/ProfileCard";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import {s} from "./App.style";
+import { s } from "./App.style";
 
 export default function App() {
   return ( //the two lines below ensure that the components being displayed show at a proper place in the device.
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1, justifyContent: "center", padding: 20  }}>
-        <ProfileCard/>
+      <SafeAreaView style={{ flex: 1, justifyContent: "center", padding: 20 }}>
+        <ProfileCard
+        firstname={"John"}
+        lastname={"Snow"}
+        age={30}
+        isOpenToWork={true}
+        car = {{brand: "Mustang", maxspeed: 200}}
+        doSomething={function(){
+        console.log("Hello!")
+        }} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- components/ProfileCard/ProfileCard.jsx
+++ components/ProfileCard/ProfileCard.jsx
@@ -2,7 +2,7 @@ import { TouchableOpacity, Text, View, Image } from "react-native";
 import { s } from "./ProfileCard.style";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
-export function ProfileCard() {
+export function ProfileCard(props) {
   return (
     <View style={s.container}>
       <View style={s.header}>
@@ -13,9 +13,9 @@ export function ProfileCard() {
           />
         </View>
         <View style={s.texts}>
-          <Text style={s.name}>Abdullah Salami</Text>
+          <Text style={s.name}>{props.firstname} {props.lastname}</Text>
           <Text>
-            Just testing react native after a long while to see how it goes
+            Just testing react native after {props.age} years to see how it goes.
           </Text>
         </View>
       </View>
